bbe9dfc80a0c26418335fe8fef6663594ca5afc6
messalina <distill@mail.ru>
2022-04-22 13:13:18 +0300

    files deleted

--- HomeWork1.3.py
+++
@@ -1,7 +0,0 @@
-n = input('Введите число')
-nn = n + n
-nnn = n + nn
-n = int(n)
-nn = int(nn)
-nnn = int(nnn)
-print(n + nn + nnn)
--- HomeWork1.4.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Введите положительное число'))
-maxCifra = 1
-while number > 0:
-    cifra = number % 10
-    if cifra > maxCifra:
-        maxCifra = cifra
-    number = number // 10
-print(maxCifra)
